9050146e9ca7e8ef3b69069dcb38192fa81dcbcc
porco <shixin@zhimajiayou.com>
2023-10-16 19:16:29 +0800

    http request template

--- demo-javassist/src/main/java/com/porco/javassist/domain/Result.java
+++ demo-javassist/src/main/java/com/porco/javassist/domain/Result.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class Result {
     private Object data;
+    private String msg;
 
     public static Result ok() {
         return new Result();
@@ -13,6 +14,12 @@ public class Result {
     public static Result of(Object o) {
         Result result = new Result();
         result.setData(o);
+        return result;
+    }
+
+    public static Result fail(String msg) {
+        Result result = new Result();
+        result.setMsg(msg);
         return result;
     }
 }
